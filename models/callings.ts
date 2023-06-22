0aa2ce13b96fe15dbbd8343cdcc46997f82b09c8
Samuel Pletain <samuel.pletain@gmail.com>
2023-06-22 13:04:52 -0600

    Updated callings model

--- models/callings.ts
+++ models/callings.ts
@@ -1,20 +1,18 @@
 import { Schema, model, ObjectId } from 'mongoose';
 
 interface Calling {
-  callingId: ObjectId
   organizationId: ObjectId
   name: string
 }
 
 const callingSchema = new Schema<Calling>({
-  callingId: {
-    type: Schema.Types.ObjectId,
-  },
   organizationId: {
     type: Schema.Types.ObjectId,
+    required: true
   },
   name: {
     type: String,
+    required: true
   },
 
 });
